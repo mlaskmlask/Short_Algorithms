81f77bda5ceab1478ac828ac18e659c4220fc354
mlaskmlask <laskowska.kadry@gmail.com>
2021-11-01 13:42:58 +0100

    Exercises3

--- src/com/company/ClosestToZero.java
+++ src/com/company/ClosestToZero.java
@@ -5,17 +5,22 @@ public class ClosestToZero {
         int[] table = {20, 7, 32, -10, -5, 15, -30};
 
         int minimumSum = table[0] + table[1];
-        int pairFirstIndex = 0;
-        int pairSecondIndex = 1;
+        int firstNumber = 0;
+        int secondNumber = 0;
 
         for (int i = 0; i < table.length; i++) {
             for (int j = i + 1; j < table.length; j++) {
                 int sum = table[i] + table[j];
                 if (Math.abs(sum) < Math.abs(minimumSum)) {
+                    firstNumber = table[i];
+                    secondNumber = table[j];
                     minimumSum = sum;
                 }
             }
         }
         System.out.println(minimumSum);
+
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
     }
 }
