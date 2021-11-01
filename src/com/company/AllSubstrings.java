5e1b5fe4fb927d5d972b1d884f5ccf2968e149e9
mlaskmlask <laskowska.kadry@gmail.com>
2021-11-01 10:46:13 +0100

    Exercises2

--- src/com/company/AllSubstrings.java
+++ src/com/company/AllSubstrings.java
@@ -1,5 +1,15 @@
 package com.company;
 
 public class AllSubstrings {
+    public static void main(String[] args) {
 
+
+        String word = "Orzeszek";
+
+        for (int i = 0; i < word.length(); i++) {
+            for (int j = i + 1; j <= word.length(); j++) {
+                System.out.println(word.substring(i, j));
+            }
+        }
+    }
 }
